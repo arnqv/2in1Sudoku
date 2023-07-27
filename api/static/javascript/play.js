e0c246a7ef05c5e3c697db50456bfa270e654080
ambhatt2 <ambhatt2@ncsu.edu>
2023-07-27 17:01:22 -0400

    fixed colrorrr

--- api/static/javascript/play.js
+++ api/static/javascript/play.js
@@ -1356,7 +1356,7 @@ function changeTheme() {
     }
     
     // else if the desired theme change is "retro", change CSS variables to the corresponding color palette
-    else if (themeid == "retro") {
+    else if (themeid == "red") {
 
         // change document CSS colors
         root.style.setProperty('--primaryColor', "#111f4d");
@@ -1371,9 +1371,14 @@ function changeTheme() {
         root.style.setProperty('--shiftColor', "#fce17a");
         root.style.setProperty('--messageTextColor', "#f6f0e8");
         root.style.setProperty('--focusText', "#F3ECE7");
-        root.style.setProperty('--highlightOpacity', "brightness(80%)");
+        root.style.setProperty('--highlightOpacity', "brightness(75%)");
         root.style.setProperty('--shiftIndication', "#e43a19");
         root.style.setProperty('--linkColor', "#e43a19");
+        root.style.setProperty('--buttonColor', "rgba(255, 17, 0)");
+        root.style.setProperty('--darkerButtonColor', "rgba(0, 255, 0, 1)");
+        root.style.setProperty('--lighterButtonColor', "rgba(250, 124, 115)");
+        root.style.setProperty('--bodyTextColor', "rgba(250, 124, 115)");
+        root.style.setProperty('--underlineColor', "rgba(255, 17, 0)");
 
         // change table colors
         root.style.setProperty('--color1', "#FFCCCC");
@@ -1390,12 +1395,11 @@ function changeTheme() {
         document.getElementById("homesquare").style.backgroundImage = "url(/static/css/images/homeRetro.png)";
 
         // store the theme "retro" in local cache
-        window.localStorage.setItem("storedTheme", "retro");
+        window.localStorage.setItem("storedTheme", "red");
     }
 
     // else the desired theme change is "light", change CSS variables to the corresponding color palette
     else {
-
         // change document CSS colors
         root.style.setProperty('--primaryColor', "#add2c9");
         root.style.setProperty('--itemBackground', "#f1ebeb");
@@ -1412,6 +1416,11 @@ function changeTheme() {
         root.style.setProperty('--highlightOpacity', "brightness(90%)");
         root.style.setProperty('--shiftIndication', "#28595c");
         root.style.setProperty('--linkColor', "#b266ff");
+        root.style.setProperty('--buttonColor', "rgba(5, 43, 255)");
+        root.style.setProperty('--darkerButtonColor', "rgba(0, 255, 0, 1)");
+        root.style.setProperty('--lighterButtonColor', "rgba(120, 140, 255)");
+        root.style.setProperty('--bodyTextColor', "rgba(120, 140, 255)");
+        root.style.setProperty('--underlineColor', "rgba(5, 43, 255)");
 
         // change table colors
         root.style.setProperty('--color1', "#FF9999");
@@ -1428,7 +1437,7 @@ function changeTheme() {
         document.getElementById("homesquare").style.backgroundImage = "url(/static/css/images/homeLight.png)";
 
         // store the theme "light" in local cache
-        window.localStorage.setItem("storedTheme", "light");
+        window.localStorage.setItem("storedTheme", "blue");
     }
 
     
