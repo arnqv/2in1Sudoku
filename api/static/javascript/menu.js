296050cb23ce13748e88fd406027950dceb4d451
ambhatt2 <ambhatt2@ncsu.edu>
2023-07-27 12:42:42 -0400

    poss

--- api/static/javascript/menu.js
+++ api/static/javascript/menu.js
@@ -32,10 +32,10 @@ function playOption() {
 function chooseOption() {
     document.getElementById("playcontainer").innerHTML = `
     <form action="/play" method="POST">
-    <button id="easy" class = "pulseText" style = "position:absolute; left:210px; top:250px; font-size: 1.9em;" name="easy" value="easy" type="submit" class="diffbutton">Easy</button>
-    <button id="medium" class = "pulseText" name="medium" value="medium" style = "position:absolute; left:210px; top:310px; font-size: 1.9em;" type="submit" class="diffbutton">Medium</button>
-    <button id="hard" class = "pulseText" style = "position:absolute; left:210px; top:370px; font-size: 1.9em;" name="hard" value="hard" type="submit" class="diffbutton">Hard</button>
-    <button id="insane" class = "pulseText" name="insane" style = "position:absolute; left:210px; top:430px; font-size: 1.9em;" value="insane" type="submit" class="diffbutton">Insane</button>
+    <button id="easy" class = "pulseText" style = "position:absolute; left:210px; top:235px; font-size: 1.9em;" name="easy" value="easy" type="submit" class="diffbutton">Easy</button>
+    <button id="medium" class = "pulseText" name="medium" value="medium" style = "position:absolute; left:210px; top:295px; font-size: 1.9em;" type="submit" class="diffbutton">Medium</button>
+    <button id="hard" class = "pulseText" style = "position:absolute; left:210px; top:355px; font-size: 1.9em;" name="hard" value="hard" type="submit" class="diffbutton">Hard</button>
+    <button id="insane" class = "pulseText" name="insane" style = "position:absolute; left:210px; top:415px; font-size: 1.9em;" value="insane" type="submit" class="diffbutton">Insane</button>
     </form>
     `;
 }
