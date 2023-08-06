80a0de65716b86ecc734037afc8a5c3a159e4860
ambhatt2 <ambhatt2@ncsu.edu>
2023-08-06 19:02:31 -0400

    fixed button

--- api/static/javascript/play.js
+++ api/static/javascript/play.js
@@ -1090,7 +1090,7 @@ function highlightNums() {
 
         // if the cell is not in "conflictArray", unhighlight the cell
         if (conflictArray.includes(i) == false) {
-            document.getElementById(i).style.backgroundColor = "none";
+            document.getElementById(i).style.backgroundColor = "black";
         }
 
         // if the cell's value includes "num"
