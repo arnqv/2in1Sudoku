2625efd03da598df395e8d7565f39b04e659086b
ambhatt2 <ambhatt2@ncsu.edu>
2023-08-09 11:55:22 -0400

    added svg

--- api/static/javascript/play.js
+++ api/static/javascript/play.js
@@ -1836,3 +1836,18 @@ function restart() {
     // indicate that shift is not pressed
     isShift = false;
 }
+
+$(document).ready(function() {
+    var state = "paused";
+    $('#pause').on('click', function() {
+      if(state == 'paused') {
+        state = "playing";
+        $("#circle").attr("class", "play");
+        $("#from_pause_to_play")[0].beginElement();
+      } else {
+        state = "paused";
+        $("#circle").attr("class", "");
+        $("#from_play_to_pause")[0].beginElement();
+      }
+    });
+  });
